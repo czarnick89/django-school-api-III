3c937ea9be51e2af085698da3e5b8d1a5dce4439
czarnick89 <nso83@hotmail.com>
2025-07-28 14:39:52 -0500

    djangoSchool7complete

--- school_proj/student_app/urls.py
+++ school_proj/student_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import all_students
+from .views import all_students, a_student
 
 urlpatterns = [
-    path('', all_students.as_view(), name='all_students')
+    path('', all_students.as_view(), name='all_students'),
+    path('<int:id>/', a_student.as_view(), name='a_student')
 ]
--- school_proj/subject_app/urls.py
+++ school_proj/subject_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import all_subjects
+from .views import all_subjects, a_subject
 
 urlpatterns = [
-    path('', all_subjects.as_view(), name='all_subjects')
+    path('', all_subjects.as_view(), name='all_subjects'),
+    path('<str:subject>/', a_subject.as_view(), name='a_subject')
 ]
